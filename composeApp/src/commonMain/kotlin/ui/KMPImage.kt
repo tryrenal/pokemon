9b119a2f015eddb7cbd7f63e4913d643aa937fa2
mhmmdrdwnsyhx <morganhydra27@gmail.com>
2025-07-02 09:23:05 +0700

    Improve card UI components and style consistency

---
+++ composeApp/src/commonMain/kotlin/ui/KMPImage.kt
@@ -0,0 +1,6 @@
+package ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+expect fun KMPImage(url: String, contentDescription: String)
---
+++ composeApp/src/iosMain/kotlin/ui/KMPImage.kt
@@ -0,0 +1,8 @@
+package ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun KMPImage(url: String, contentDescription: String) {
+    // SwiftUI image loader
+}
